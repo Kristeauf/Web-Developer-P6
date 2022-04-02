39fdffbe06229d672e9afc4fb4cc62b3c87ccf00
Kristeauf <kristeauf@gmail.com>
2022-04-02 16:42:04 +0200

    mise a jour

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -4,8 +4,8 @@ const sauceCtrl = require("../controllers/SaucesController");
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 
-router.post("/", auth, multer, sauceCtrl.createSauce,); 
-router.post('/:id/like', auth, sauceCtrl.likeOrDislike)
+ router.post("/", auth, multer, sauceCtrl.createSauce,); 
+ router.post('/:id/like', auth, sauceCtrl.likeOrDislike)
 router.get("/", auth, sauceCtrl.getAllSauces);
 
 router.get("/:id", auth,sauceCtrl.getOneSauce);
